b0f8d7c3006e41197123680256df557a7b90207c
Komal Sharma <komal45812@example.com>
2025-07-06 18:37:25 +0530

    changing url

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ const mongoose=require('mongoose');
 mongoose.set('strictQuery',false);
 const connectDB=async()=>{
     try{
+       
         const conn=await mongoose.connect(process.env.mongodb_uri);
         console.log(`database connected: ${conn.connection.host}`);
     }catch(error){
